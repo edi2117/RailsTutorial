7314763617e39262552e3270409b7209342aff86
Nick Collins <nickc423@gmail.com>
2015-06-30 05:05:21 +0000

    Add account activations

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -11,9 +11,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name: "",
-                               email: "user@invalid",
-                               password:              "foo", 
-                               password_confirmation: "bar" }
+                                    email: "user@invalid",
+                                    password:              "foo", 
+                                    password_confirmation: "bar" }
     assert_template 'users/edit'
   end
   
@@ -24,9 +24,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name: name,
-                               email: email,
-                               password:              "", 
-                               password_confirmation: "" }
+                                    email: email,
+                                    password:              "", 
+                                    password_confirmation: "" }
     assert_not flash.empty?
     assert_redirected_to @user
     @user.reload
