6286203d151a1c513840ce0b492bf64f7b655518
nicksc423 <nicksc423@gmail.com>
2017-01-06 13:39:29 -0800

    Fixing bug where tests would exit with errors even tho successful

--- run_tests.sh
+++ run_tests.sh
@@ -5,9 +5,12 @@ set -e
 if [ "$TEST_TYPE" = "post" ]
 then
   rake test:postdeployment
+  exit 0
 elif [ "$TEST_TYPE" = "pre" ]
 then
   rake test:predeployment
+  exit 0
 else
   rake test
+  exit 0
 fi
